b2ed3bd0094971aede1b599867063a79e7236ab3
stevebottos <steve.bottos@hotmail.ca>
2020-05-22 14:08:55 -0700

    Adding some stuff

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from sparknlp.base import *
 from sparknlp.annotator import *
 from pyspark.ml import Pipeline
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 import sparknlp
 
+# spark = sparknlp.start()
+# Had to start session manuallt rather than with sparknlp.start() to tune memory preferences
 spark = SparkSession.builder \
     .appName("Spark NLP")\
     .config("spark.driver.memory","1G")\
@@ -11,9 +14,46 @@ spark = SparkSession.builder \
     .config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.0")\
     .config("spark.kryoserializer.buffer.max", "500M")\
     .getOrCreate()
+
 print("\n\n\n#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#\n\n\n")
 
 trainDataset = spark.read \
       .option("header", True) \
       .csv("nlp_dat.csv")
-trainDataset.show(10, truncate=50)
+
+document = DocumentAssembler()\
+    .setInputCol("description")\
+    .setOutputCol("document")
+
+# use = UniversalSentenceEncoder.pretrained(name="tfhub_use", lang="en")\
+#  .setInputCols(["document"])\
+#  .setOutputCol("sentence_embeddings")
+
+use = UniversalSentenceEncoder.load("model/")\
+ .setInputCols(["document"])\
+ .setOutputCol("sentence_embeddings")
+
+
+# bert = BertEmbeddings.load('/tmp/bert_base_cased_en_2.4.0_2.4_1580579557778')\
+# .setInputCols(["sentence",'token'])\
+# .setOutputCol("bert")\
+# .setCaseSensitive(False)\
+# .setPoolingLayer(0)
+
+# classifier_dl = ClassifierDLApproach()\
+#   .setInputCols(["sentence_embeddings"])\
+#   .setOutputCol("class")\
+#   .setLabelColumn("category")\
+#   .setMaxEpochs(1)\
+#   .setEnableOutputLogs(True)
+#
+# use_clf_pipeline = Pipeline(
+#     stages = [
+#         document,
+#         use,
+#         classifier_dl
+#     ])
+#
+# use_pipelineModel = use_clf_pipeline.fit(trainDataset)
+
+print("\n\n\n#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#\n\n\n")
